0a93bdbb232d1f6ca688a6ea35ae7a5359f28eb8
anabbott <anabbott@smu.edu>
2019-01-25 18:31:29 -0600

    working on documentation and tests

--- R/data_prep.R
+++ R/data_prep.R
@@ -5,9 +5,10 @@
 #' planList()
 
 planList <- function() {
-  dw <- config::get("datawarehouse")
+  dw <- get("dw")
+  # dw <- config::get(config = "datawarehouse")
   con <- RPostgres::dbConnect(
-    RPostgreg::Postgres(),
+    RPostgres::Postgres(),
     dbname = trimws(dw$path),
     host = dw$hostname,
     port = dw$port,
@@ -32,8 +33,7 @@ planList <- function() {
   # fetches the results
   plans <- RPostgres::dbFetch(res)
   p_list <- plans %>%
-    dplyr::mutate_if(sapply(plans, is.character), as.factor) %>%
-    janitor::clean_names(.)
+    janitor::clean_names()
   # clears the results
   RPostgres::dbClearResult(res)
   # closes the connection
@@ -49,15 +49,10 @@ planList <- function() {
 
 pullData <-
   function(pl, plan_name = "Texas Employees Retirement System") {
-    require(RPostgres)
-    require(httr)
-    require(tidyverse)
-    require(janitor)
-    require(config)
-
-    dw <- config::get("datawarehouse")
-    con <- dbConnect(
-      Postgres(),
+    dw <- get("dw")
+    #dw <- config::get("datawarehouse")
+    con <- RPostgres::dbConnect(
+      RPostgres::Postgres(),
       dbname = trimws(dw$path),
       host = dw$hostname,
       port = dw$port,
@@ -91,19 +86,19 @@ pullData <-
 
     plan_id <- pl$id[pl$display_name == plan_name]
 
-    result <- dbSendQuery(con, query)
-    dbBind(result, list(plan_id))
-    all_data <- dbFetch(result) %>%
-      clean_names()
-    dbClearResult(result)
-    dbDisconnect(con)
+    result <- RPostgres::dbSendQuery(con, query)
+    RPostgres::dbBind(result, list(plan_id))
+    all_data <- RPostgres::dbFetch(result) %>%
+      janitor::clean_names()
+    RPostgres::dbClearResult(result)
+    RPostgres::dbDisconnect(con)
     all_data %>%
-      group_by_at(vars(-attribute_value)) %>%  # group by everything other than the value column.
-      mutate(row_id = 1:n()) %>%
-      ungroup() %>%  # build group index
-      spread(attribute_name, attribute_value, convert = TRUE) %>%    # spread
-      select(-row_id) %>%  # drop the index
-      clean_names()
+      dplyr::group_by_at(dplyr::vars(-.data$attribute_value)) %>%  # group by everything other than the value column.
+      dplyr::mutate(row_id = 1:dplyr::n()) %>%
+      dplyr::ungroup() %>%  # build group index
+      tidyr::spread(.data, .data$attribute_name, .data$attribute_value, convert = TRUE) %>%    # spread
+      dplyr::select(-.data$row_id) %>%  # drop the index
+      janitor::clean_names()
   }
 
 ####################################################################
@@ -112,11 +107,8 @@ pullData <-
 # Usage: allWide <- loadData('data/NorthCarolina_PensionDatabase_TSERS.xlsx')
 
 loadData <- function(filename) {
-  require(tidyverse)
-  require(janitor)
-  require(readxl)
-  read_excel(filename, col_types = "numeric") %>%
-    clean_names()
+  readxl::read_excel(filename, col_types = "numeric") %>%
+    janitor::clean_names()
 }
 
 ####################################################################
@@ -138,26 +130,26 @@ modData <- function(wide_data,
   .year_var = "year",
   .aal_var = "actuarial_accrued_liabilities_under_gasb_standards",
   .asset_var = "actuarial_assets_under_gasb_standards",
-  base = 1000) {
-  require(tidyverse)
-  year_var <- sym(.year_var)
-  aal_var <- sym(.aal_var)
-  asset_var <- sym(.asset_var)
+  base = 1000){
+
+  year_var <- rlang::sym(.year_var)
+  aal_var <- rlang::sym(.aal_var)
+  asset_var <- rlang::sym(.asset_var)
 
   wide_data %>%
-    select(year = !!year_var, actuarial_assets = !!asset_var, aal = !!aal_var) %>%
-    mutate(
-      uaal = as.numeric(aal) - as.numeric(actuarial_assets),
+    dplyr::select(year = !!year_var, actuarial_assets = !!asset_var, aal = !!aal_var) %>%
+    dplyr::mutate(
+      uaal = as.numeric(.data$aal) - as.numeric(.data$actuarial_assets),
       # create a UAAL column as AAL-Actuarial Assets
-      funded_ratio = as.numeric(actuarial_assets) / as.numeric(aal),
+      funded_ratio = as.numeric(.data$actuarial_assets) / as.numeric(.data$aal),
       # create a fundedRatio column as Actuarial Assets divided by AAL
     ) %>%
-    mutate(
-      actuarial_assets = as.numeric(actuarial_assets) * base,
-      aal = as.numeric(aal) * base,
-      uaal = uaal * base
+    dplyr::mutate(
+      actuarial_assets = as.numeric(.data$actuarial_assets) * base,
+      aal = as.numeric(.data$aal) * base,
+      uaal = .data$uaal * base
     ) %>%
-    drop_na()
+    tidyr::drop_na()
 }
 
 
@@ -169,11 +161,7 @@ modData <- function(wide_data,
 # Usage: modGraph(data)
 
 modGraph <- function(data) {
-  require(tidyverse)
-  require(ggthemes)
-  require(extrafont)
-  require(scales)
-
+  reasonTheme <- get("reasonTheme")
   # extrapolate between years linearly
   extrapo <- approx(data$year, data$uaal, n = 10000)
   extrapo2 <- approx(data$year, data$funded_ratio, n = 10000)
@@ -187,33 +175,33 @@ modGraph <- function(data) {
   graph$sign[graph$uaal >= 0] <- "positive"
   graph$sign[graph$uaal < 0] <- "negative"
 
-  ggplot(graph, aes(x = year)) +
+  ggplot2::ggplot(graph, ggplot2::aes(x = graph$year)) +
     # area graph using pos/neg for fill color
-    geom_area(aes(y = uaal, fill = sign)) +
+    ggplot2::geom_area(ggplot2::aes(y = graph$uaal, fill = graph$sign)) +
     # line tracing the area graph
-    geom_line(aes(y = uaal)) +
+    ggplot2::geom_line(ggplot2::aes(y = graph$uaal)) +
     # line with funded ratio
-    geom_line(aes(y = funded_ratio * (max(graph$uaal))), color = "#3300FF", size = 1) +
+    ggplot2::geom_line(ggplot2::aes(y = graph$funded_ratio * (max(graph$uaal))), color = "#3300FF", size = 1) +
     # axis labels
-    labs(y = "Unfunded Accrued Actuarial Liabilities", x = NULL) +
+    ggplot2::labs(y = "Unfunded Accrued Actuarial Liabilities", x = NULL) +
 
     # colors assigned to pos, neg
-    scale_fill_manual(values = c("negative" = "#669900", "positive" = "#CC0000")) +
+    ggplot2::scale_fill_manual(values = c("negative" = "#669900", "positive" = "#CC0000")) +
 
     # sets the y-axis scale
-    scale_y_continuous(
+    ggplot2::scale_y_continuous(
       # creates 10 break points for labels
-      breaks = pretty_breaks(n = 10),
+      breaks = scales::pretty_breaks(n = 10),
       # changes the format to be dollars, without cents, scaled to be in billions
-      labels = dollar_format(
+      labels = scales::dollar_format(
         prefix = "$",
         scale = (1e-9),
         largest_with_cents = 1
       ),
       # defines the right side y-axis as a transformation of the left side axis, maximum UAAL = 100%, sets the breaks, labels
-      sec.axis = sec_axis(
+      sec.axis = ggplot2::sec_axis(
         ~ . / (max(graph$uaal) / 100),
-        breaks = pretty_breaks(n = 10),
+        breaks = scales::pretty_breaks(n = 10),
         name = "Funded Ratio",
         labels = function(b) {
           paste0(round(b, 0), "%")
@@ -224,7 +212,7 @@ modGraph <- function(data) {
     ) +
 
     # sets the x-axis scale
-    scale_x_continuous( # sets the years breaks to be every 2 years
+    ggplot2::scale_x_continuous( # sets the years breaks to be every 2 years
       breaks = round(seq(min(graph$year), max(graph$year), by = 2), 1),
       expand = c(0, 0)
     ) +
@@ -240,20 +228,18 @@ modGraph <- function(data) {
 # Usage: modTable(data)
 
 modTable <- function(data) {
-  require(DT)
-  require(tidyverse)
 
   data <- data %>%
     # give the columns pretty names
-    rename(
-      "Year" = year,
-      "Actuarial Assets" = actuarial_assets,
-      "Actuarial Accrued Liabilities" = aal,
-      "Unfunded Actuarial Accrued Liabilities" = uaal,
-      "Funded Ratio" = funded_ratio
+    dplyr::rename(
+      "Year" = .data$year,
+      "Actuarial Assets" = .data$actuarial_assets,
+      "Actuarial Accrued Liabilities" = .data$aal,
+      "Unfunded Actuarial Accrued Liabilities" = .data$uaal,
+      "Funded Ratio" = .data$funded_ratio
     )
   # create a datatable
-  datatable(
+  DT::datatable(
     data,
     # add buttons for export, etc.
     extensions = c("Buttons"),
@@ -286,8 +272,8 @@ modTable <- function(data) {
       )
     )
   ) %>%
-    formatCurrency(c(2:4)) %>%
-    formatPercentage(5, 2)
+    DT::formatCurrency(c(2:4)) %>%
+    DT::formatPercentage(5, 2)
 }
 
 ####################################################################
@@ -297,25 +283,20 @@ modTable <- function(data) {
 #     ylab: The y-axis label, default set
 # Usage: glGraph(filename = 'data/Graph 1.csv')
 
+
 glGraph <-
   function(filename, ylab = "Changes in Unfunded Liability (in Billions)") {
-    require(ggplot2)
-    require(tidyverse)
-
-    graph1 <- read_csv(filename) %>% # load data from csv file
-      gather("label", "value") %>% # put in long format with label-value pairs
-      mutate(label = str_wrap(label, 8)) %>% # wrap the label names to clean up axis labels
-      mutate(label = str_to_title(label)) %>% # properly capitalize the labels
+    reasonTheme <- get("reasonTheme")
+    graph1 <- readr::read_csv(filename) %>% # load data from csv file
+      tidyr::gather("label", "value") %>% # put in long format with label-value pairs
+      dplyr::mutate(label = stringr::str_wrap(.data$label, 8)) %>% # wrap the label names to clean up axis labels
+      dplyr::mutate(label = stringr::str_to_title(.data$label)) %>% # properly capitalize the labels
       # assign pos/neg/total to the values for fill color
-      mutate(
-        sign = case_when(
-          value >= 0 ~ "positive",
-          value < 0 ~ "negative"
-        )
-      ) %>%
-      mutate(sign = case_when(label == "Total" ~ "total", TRUE ~ sign)) %>%
-      mutate(sign = factor(sign, levels = c("total", "negative", "positive"))) %>%
-      mutate(label = factor(label, levels = label[order(sign, value, label, decreasing = TRUE)], ordered = TRUE))
+      dplyr::mutate(sign = dplyr::case_when(.data$value >= 0 ~ "positive",
+        .data$value < 0 ~ "negative")) %>%
+      dplyr::mutate(sign = dplyr::case_when(.data$label == "Total" ~ "total", TRUE ~ .data$sign)) %>%
+      dplyr::mutate(sign = factor(.data$sign, levels = c("total", "negative", "positive"))) %>%
+      dplyr::mutate(label = factor(.data$label, levels = .data$label[order(.data$sign, .data$value, .data$label, decreasing = TRUE)], ordered = TRUE))
 
     # assign colors to go with signs
     fill_colors <- c(
@@ -325,19 +306,18 @@ glGraph <-
     )
 
     # create plot
-    ggplot(graph1, aes(x = label, y = value)) +
-      geom_col(width = 0.75, aes(fill = sign)) +
-      geom_hline(yintercept = 0, color = "black") +
-      scale_fill_manual(values = fill_colors) +
-      scale_y_continuous(breaks = pretty_breaks(), labels = dollar_format(prefix = "$")) +
-      ylab(ylab) +
+    ggplot2::ggplot(graph1, ggplot2::aes(x = graph1$label, y = graph1$value)) +
+      ggplot2::geom_col(width = 0.75, ggplot2::aes(fill = graph1$sign)) +
+      ggplot2::geom_hline(yintercept = 0, color = "black") +
+      ggplot2::scale_fill_manual(values = fill_colors) +
+      ggplot2::scale_y_continuous(breaks = scales::pretty_breaks(), labels = scales::dollar_format(prefix = "$")) +
+      ggplot2::ylab(ylab) +
       reasonTheme +
-      theme(
-        axis.line.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.text.x = element_text(angle = 0)
+      ggplot2::theme(
+        axis.line.x = ggplot2::element_blank(),
+        axis.ticks.x = ggplot2::element_blank(),
+        axis.text.x = ggplot2::element_text(angle = 0)
       )
-    # ggsave("graph1.2.png", width = 9, height = 5.33)
   }
 
 ####################################################################
@@ -366,41 +346,38 @@ selectedData <- function(wide_data,
   .adec_var = "employer_annual_required_contribution",
   .emp_cont_var = "employer_contributions",
   .payroll_var = "covered_payroll") {
-  require(tidyverse)
-  require(lubridate)
-  require(janitor)
 
-  date_var <- sym(.date_var)
-  aal_var <- sym(.aal_var)
-  asset_var <- sym(.asset_var)
-  adec_var <- sym(.adec_var)
-  emp_cont_var <- sym(.emp_cont_var)
-  payroll_var <- sym(.payroll_var)
+  date_var <- rlang::sym(.date_var)
+  aal_var <- rlang::sym(.aal_var)
+  asset_var <- rlang::sym(.asset_var)
+  adec_var <- rlang::sym(.adec_var)
+  emp_cont_var <- rlang::sym(.emp_cont_var)
+  payroll_var <- rlang::sym(.payroll_var)
 
   wide_data %>%
-    mutate(
+    dplyr::mutate(
       date = !!date_var
     ) %>%
-    mutate(
-      year = year(excel_numeric_to_date(as.numeric(date))),
-      valuation_date = excel_numeric_to_date(as.numeric(date))
+    dplyr::mutate(
+      year = lubridate::year(janitor::excel_numeric_to_date(as.numeric(.data$date))),
+      valuation_date = janitor::excel_numeric_to_date(as.numeric(.data$date))
     ) %>%
-    select(
-      year,
-      valuation_date,
+    dplyr::select(
+      .data$year,
+      .data$valuation_date,
       actuarial_assets = !!asset_var,
       aal = !!aal_var,
       adec = !!adec_var,
       emp_cont = !!emp_cont_var,
       payroll = !!payroll_var
     ) %>%
-    mutate(
-      uaal = as.numeric(aal) - as.numeric(actuarial_assets),
-      funded_ratio = as.numeric(actuarial_assets) / as.numeric(aal),
-      adec_contribution_rates = as.numeric(adec) / as.numeric(payroll),
-      actual_contribution_rates = as.numeric(emp_cont) / as.numeric(payroll)
+    dplyr::mutate(
+      uaal = as.numeric(.data$aal) - as.numeric(.data$actuarial_assets),
+      funded_ratio = as.numeric(.data$actuarial_assets) / as.numeric(.data$aal),
+      adec_contribution_rates = as.numeric(.data$adec) / as.numeric(.data$payroll),
+      actual_contribution_rates = as.numeric(.data$emp_cont) / as.numeric(.data$payroll)
     ) %>%
-    drop_na()
+    tidyr::drop_na()
 }
 
 ####################################################################
@@ -417,20 +394,17 @@ contGraph <- function(data,
   label1 = NULL,
   label2 = NULL,
   label3 = NULL) {
-  require(ggplot2)
-  require(tidyverse)
-  require(scales)
 
+  reasonTheme <- get("reasonTheme")
   graph <- data %>%
-    select(
-      year,
-      y1,
-      y2,
-      y3
+    dplyr::select(
+      .data$year,
+      label1 = .data$y1,
+      label2 = .data$y2,
+      label3 = .data$y3
     ) %>%
-    mutate_all(funs(as.numeric)) %>%
-    rename(label1 = y1, label2 = y2, label3 = y3) %>%
-    gather(key = keys, value = amount, -year)
+    dplyr::mutate_all(dplyr::funs(as.numeric)) %>%
+    tidyr::gather(key = .data$keys, value = .data$amount, -.data$year)
 
   lineColors <- c(
     y1 = "#FF6633",
@@ -444,27 +418,27 @@ contGraph <- function(data,
     label3
   )
 
-  ggplot(graph, aes(x = year)) +
-    geom_line(aes(y = amount * 100, color = keys), size = 2) +
-    scale_fill_manual(values = lineColors) +
-    geom_hline(yintercept = 0, color = "black") +
+  ggplot2::ggplot(graph, ggplot2::aes(x = graph$year)) +
+    ggplot2::geom_line(ggplot2::aes(y = graph$amount * 100, color = graph$keys), size = 2) +
+    ggplot2::scale_fill_manual(values = lineColors) +
+    ggplot2::geom_hline(yintercept = 0, color = "black") +
 
-    scale_y_continuous(
-      breaks = pretty_breaks(10),
+    ggplot2::scale_y_continuous(
+      breaks = scales::pretty_breaks(10),
       labels = function(b) {
         paste0(round(b, 0), "%")
       }
     ) +
 
-    scale_x_continuous(breaks = pretty_breaks(10)) +
+    ggplot2::scale_x_continuous(breaks = scales::pretty_breaks(10)) +
 
-    ylab(labelY) +
-    scale_color_discrete(labels = labs) +
+    ggplot2::ylab(labelY) +
+    ggplot2::scale_color_discrete(labels = labs) +
 
     reasonTheme +
-    theme(
+    ggplot2::theme(
       legend.justification = c(1, 1),
       legend.position = c(0.5, 1),
-      legend.title = element_blank()
+      legend.title = ggplot2::element_blank()
     )
 }
